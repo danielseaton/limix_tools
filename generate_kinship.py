258787dc0981db6b2a94a73823956f24a3e5ff1f
danielseaton <daniel.seaton87@gmail.com>
2017-08-23 14:17:45 +0100

    chunk snps to decrease memory usage

--- generate_kinship.py
+++ generate_kinship.py
@@ -2,25 +2,33 @@ import numpy as np
 import pandas as pd
 import scipy.stats
 import re
-import glob
 import scipy
 import limix
 from sklearn.preprocessing import Imputer
 
 def generate_kinship(snp_df):
     fill_NaN = Imputer(missing_values=np.nan, strategy='mean', axis=0)
-    
+
     snp_df = snp_df.dropna(how='all',axis=1)
     snp_df = snp_df.dropna(how='all',axis=0)
     snp_df = pd.DataFrame(data=fill_NaN.fit_transform(snp_df),columns=snp_df.columns,index=snp_df.index)
     std = snp_df.apply(np.std,axis=0)
     #keep snps with some variation 
     snp_df = snp_df.loc[:,std>0]
-    X = snp_df.values
-    X -= X.mean(0)
-    X /= X.std(0)
-    X /= np.sqrt(X.shape[1])
-    K = X.dot(X.T)
+    nI = len(snp_df.index)
+    K = np.zeros((nI,nI))
+    
+    chunk_size = 100000
+    nChunks = len(snp_df.columns)/chunk_size + 1
+    for idx in range(nChunks):
+        start = idx*chunk_size
+        stop = (idx+1)*chunk_size
+        X = snp_df.values[:,start:stop]
+        X -= X.mean(0)
+        X /= X.std(0)
+        Ktemp = X.dot(X.T)
+        K += Ktemp
+    K /= snp_df.shape[1]
     index = snp_df.index
     kinship_df = pd.DataFrame(data=K,index=index,columns=index)
     return kinship_df
