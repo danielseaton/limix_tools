69ff1fe5a65dafff1d2451f65bc8c1c0dd5ddff6
danielseaton <daniel.seaton87@gmail.com>
2017-09-05 11:54:57 +0100

    PEP8 compliance

--- generate_covariance.py
+++ generate_covariance.py
@@ -6,48 +6,52 @@ import scipy
 import limix
 from sklearn.preprocessing import Imputer
 
-def generate_cis_kinship(bed,fam,bim,chrom,start,end,window_size):
-    lowest = min([start,end])
-    highest = max([start,end])
-    cis_snp_query = bim.query("chrom == '%s' & pos > %d & pos < %d" % (chrom, lowest-window_size, highest+window_size))
+
+def generate_cis_kinship(bed, fam, bim, chrom, start, end, window_size):
+    lowest = min([start, end])
+    highest = max([start, end])
+    cis_snp_query = bim.query("chrom == '%s' & pos > %d & pos < %d" % (
+        chrom, lowest - window_size, highest + window_size))
     iid_idxs = fam.index
     snp_idxs = cis_snp_query['i'].values
-    if len(snp_idxs)==0:
+    if len(snp_idxs) == 0:
         return None
-    kinship_cis_df = generate_kinship(bed,fam,snp_idxs,iid_idxs)
-    index = pd.Index(fam.loc[kinship_cis_df.index,'iid'])
+    kinship_cis_df = generate_kinship(bed, fam, snp_idxs, iid_idxs)
+    index = pd.Index(fam.loc[kinship_cis_df.index, 'iid'])
     kinship_cis_df.index = index
     kinship_cis_df.columns = index
     return kinship_cis_df
 
 
-def generate_kinship(bed,fam,snp_idxs,iid_idxs,chunk_size=100000):
+def generate_kinship(bed, fam, snp_idxs, iid_idxs, chunk_size=100000):
     assert(all([x in fam.index for x in iid_idxs]))
 
     fill_NaN = Imputer(missing_values=np.nan, strategy='mean', axis=0)
 
     nI = len(iid_idxs)
-    K = np.zeros((nI,nI))
-    
-    nChunks = nI/chunk_size + 1
+    K = np.zeros((nI, nI))
+
+    nChunks = nI / chunk_size + 1
     snp_count = 0
     for idx in range(nChunks):
-        start = idx*chunk_size
-        stop = (idx+1)*chunk_size
+        start = idx * chunk_size
+        stop = (idx + 1) * chunk_size
         snp_idxs_subset = snp_idxs[start:stop]
 
-        #limit snp_df to snp subset, and individual subset
-        snp_df = pd.DataFrame(data=bed[snp_idxs_subset,:].compute().transpose(),index=fam.index,columns=snp_idxs_subset)
-        snp_df = snp_df.loc[iid_idxs,:]
+        # limit snp_df to snp subset, and individual subset
+        snp_df = pd.DataFrame(data=bed[snp_idxs_subset, :].compute(
+        ).transpose(), index=fam.index, columns=snp_idxs_subset)
+        snp_df = snp_df.loc[iid_idxs, :]
 
-        #drop snps that are all na
-        snp_df = snp_df.dropna(how='all',axis=0)
-        #impute missing snps
-        snp_df = pd.DataFrame(data=fill_NaN.fit_transform(snp_df),columns=snp_df.columns,index=snp_df.index)
-        #remove snps that don't vary 
-        std = snp_df.apply(np.std,axis=0)
-        snp_df = snp_df.loc[:,std>0]
-        #number of snps we're using after dropping some (should be close to chunk_size)
+        # drop snps that are all na
+        snp_df = snp_df.dropna(how='all', axis=0)
+        # impute missing snps
+        snp_df = pd.DataFrame(data=fill_NaN.fit_transform(
+            snp_df), columns=snp_df.columns, index=snp_df.index)
+        # remove snps that don't vary
+        std = snp_df.apply(np.std, axis=0)
+        snp_df = snp_df.loc[:, std > 0]
+        # number of snps we're using after dropping some (should be close to chunk_size)
         snp_count += len(snp_df.columns)
 
         X = snp_df.values
@@ -57,13 +61,14 @@ def generate_kinship(bed,fam,snp_idxs,iid_idxs,chunk_size=100000):
         K += Ktemp
 
     K /= snp_count
-    kinship_df = pd.DataFrame(data=K,index=iid_idxs,columns=iid_idxs)
+    kinship_df = pd.DataFrame(data=K, index=iid_idxs, columns=iid_idxs)
     return kinship_df
+
 
 def generate_covariance_dict(metadata_df):
     '''Return a dictionary of random effect covariance matrices based on a metadata dataframe.'''
 
-    #Drop rows with any NA values in the metadata_df
+    # Drop rows with any NA values in the metadata_df
     metadata_df.dropna(inplace=True)
 
     selected_columns = list(metadata_df.columns)
@@ -72,13 +77,14 @@ def generate_covariance_dict(metadata_df):
     for column_name in selected_columns:
         random_effect_mat = []
         for categorical_value in metadata_df[column_name]:
-            vector_of_matches = metadata_df[column_name].map(lambda x : int(x==categorical_value)).values
-            if sum(vector_of_matches)==len(vector_of_matches):
+            vector_of_matches = metadata_df[column_name].map(
+                lambda x: int(x == categorical_value)).values
+            if sum(vector_of_matches) == len(vector_of_matches):
                 print 'All samples are identical in {}'.format(column_name)
             random_effect_mat.append(vector_of_matches)
         random_effect_mat = np.array(random_effect_mat)
-        random_effect_df = pd.DataFrame(data=random_effect_mat,index=metadata_df.index,columns=metadata_df.index)
+        random_effect_df = pd.DataFrame(
+            data=random_effect_mat, index=metadata_df.index, columns=metadata_df.index)
         random_effect_dict[column_name] = random_effect_df
 
     return random_effect_dict
-
--- variance_analysis.py
+++ variance_analysis.py
@@ -7,63 +7,76 @@ import statsmodels.nonparametric.smoothers_lowess
 import random
 from generate_covariance import generate_covariance_dict
 
-def run_variance_analysis(quant_df,metadata_df,transform_fcn=np.log2):
+
+def run_variance_analysis(quant_df, metadata_df, transform_fcn=np.log2):
     '''A function to perform variance decomposition, as well as computing overdispersion
     and mean abundance statistics.'''
-    #Drop rows with any NA values in the metadata_df
-    metadata_df.dropna(inplace=True)    
+    # Drop rows with any NA values in the metadata_df
+    metadata_df.dropna(inplace=True)
     random_effect_dict = generate_covariance_dict(metadata_df)
 
     selected_columns = random_effect_dict.keys()
-    samples_w_metadata = list(set.intersection([set(x.index) for x in random_effect_dict.values()]))
+    samples_w_metadata = list(set.intersection(
+        [set(x.index) for x in random_effect_dict.values()]))
     print 'Running variance decomposition for: {}'.format(selected_columns)
 
-    #calculate variance components
-    var_df = pd.DataFrame(index = quant_df.index,columns=selected_columns+['residual'])
+    # calculate variance components
+    var_df = pd.DataFrame(index=quant_df.index,
+                          columns=selected_columns + ['residual'])
 
-    var_component_names = selected_columns+['residual']
+    var_component_names = selected_columns + ['residual']
 
-    for idx,feature_id in enumerate(quant_df.index):
-        phenotype_ds = transform_fcn(quant_df.loc[feature_id,:])
-        var_df.loc[feature_id,var_component_names] = variance_decomposition(phenotype_ds,random_effect_dict)
+    for idx, feature_id in enumerate(quant_df.index):
+        phenotype_ds = transform_fcn(quant_df.loc[feature_id, :])
+        var_df.loc[feature_id, var_component_names] = variance_decomposition(
+            phenotype_ds, random_effect_dict)
 
-    var_df['variance'] = quant_df.apply(lambda x: np.var(x.dropna()),axis=1)
-    var_df['mean'] = quant_df.apply(lambda x: np.mean(x.dropna()),axis=1)
-    var_df['overdispersion'] = calculate_empirical_overdispersion(var_df['mean'].values,var_df['variance'].values,transform_fcn)
-    var_df['overdispersion_rank'] = rank_series_w_nans(var_df['overdispersion'])
+    var_df['variance'] = quant_df.apply(lambda x: np.var(x.dropna()), axis=1)
+    var_df['mean'] = quant_df.apply(lambda x: np.mean(x.dropna()), axis=1)
+    var_df['overdispersion'] = calculate_empirical_overdispersion(
+        var_df['mean'].values, var_df['variance'].values, transform_fcn)
+    var_df['overdispersion_rank'] = rank_series_w_nans(
+        var_df['overdispersion'])
     var_df['mean_rank'] = rank_series_w_nans(var_df['mean'])
     return var_df
+
 
 def rank_series_w_nans(dataseries):
     '''Rank a dataseries from 0 to 1, keeping nans as nans.'''
     dataseries_dropped = dataseries.dropna()
-    ranked_vals_dropped = dataseries_dropped.rank()/float(len(dataseries_dropped.index))
-    ranked_vals = pd.Series(data=np.nan,index=dataseries.index)
+    ranked_vals_dropped = dataseries_dropped.rank(
+    ) / float(len(dataseries_dropped.index))
+    ranked_vals = pd.Series(data=np.nan, index=dataseries.index)
     ranked_vals.loc[ranked_vals_dropped.index] = ranked_vals_dropped
     return ranked_vals
 
 
-def calculate_empirical_overdispersion(mean,variance,transform_fcn):
+def calculate_empirical_overdispersion(mean, variance, transform_fcn):
     mean = transform_fcn(mean)
     variance = transform_fcn(variance)
-    lowess = statsmodels.nonparametric.smoothers_lowess.lowess(variance,mean,frac=0.1,return_sorted=False)
-    overdispersion = variance-lowess
+    lowess = statsmodels.nonparametric.smoothers_lowess.lowess(
+        variance, mean, frac=0.1, return_sorted=False)
+    overdispersion = variance - lowess
     return overdispersion
 
-def run_variance_analysis_cross_validation(quant_df,metadata_df,transform_fcn=np.log2,cv_fraction=0.2):
+
+def run_variance_analysis_cross_validation(quant_df, metadata_df, transform_fcn=np.log2, cv_fraction=0.2):
     metadata_df.dropna(inplace=True)
-    samples = list(set(metadata_df.index)&set(quant_df.columns))
+    samples = list(set(metadata_df.index) & set(quant_df.columns))
     random.shuffle(samples)
     nS = len(samples)
-    nLeftOut = int(cv_fraction*nS)
-    nRuns = nS/nLeftOut
-    print nS,nLeftOut,nRuns
-    sample_subsets = [samples[:x*nLeftOut]+samples[(x+1)*nLeftOut:] for x in range(nRuns)]
+    nLeftOut = int(cv_fraction * nS)
+    nRuns = nS / nLeftOut
+    print nS, nLeftOut, nRuns
+    sample_subsets = [samples[:x * nLeftOut] +
+                      samples[(x + 1) * nLeftOut:] for x in range(nRuns)]
     print len(sample_subsets)
-    var_df_list = [run_variance_analysis(quant_df.loc[:,x],metadata_df.loc[x,:],transform_fcn=transform_fcn) for x in sample_subsets]
+    var_df_list = [run_variance_analysis(
+        quant_df.loc[:, x], metadata_df.loc[x, :], transform_fcn=transform_fcn) for x in sample_subsets]
     return var_df_list
 
-def variance_decomposition(phenotype_ds,random_effect_dict):
+
+def variance_decomposition(phenotype_ds, random_effect_dict):
     '''phenotype_ds is a pandas DataSeries.
        random_effect_dict is a dictionary of pandas DataFrames.'''
 
@@ -73,9 +86,9 @@ def variance_decomposition(phenotype_ds,random_effect_dict):
 
     phenotype_ds = phenotype_ds.dropna()
 
-    samples = list(set(phenotype_ds.index)&set(samples_w_metadata))
-    if len(samples)==0:
-        var_ds = pd.Series(data=np.nan,index=var_component_names)
+    samples = list(set(phenotype_ds.index) & set(samples_w_metadata))
+    if len(samples) == 0:
+        var_ds = pd.Series(data=np.nan, index=var_component_names)
         return var_ds
 
     # variance component model
@@ -83,17 +96,18 @@ def variance_decomposition(phenotype_ds,random_effect_dict):
     # add intercept
     vc.addFixedEffect()
     for key in random_effect_dict.keys():
-        random_effect_matrix = random_effect_dict[key].loc[samples,samples].values
+        random_effect_matrix = random_effect_dict[key].loc[samples,
+                                                           samples].values
         vc.addRandomEffect(K=random_effect_matrix)
     vc.addRandomEffect(is_noise=True)
     try:
         vc.optimize()
         var_data = vc.getVarianceComps()[0]
-        var_ds = pd.Series(data=var_data,index=var_component_names)
-        var_ds = var_ds/var_ds.sum()
+        var_ds = pd.Series(data=var_data, index=var_component_names)
+        var_ds = var_ds / var_ds.sum()
     except np.linalg.linalg.LinAlgError:
-        #This error is raised when the covariance of the phenotype is not positive definite
+        # This error is raised when the covariance of the phenotype is not positive definite
         # (e.g. if it is all zeros)
-        var_ds = pd.Series(data=np.nan,index=var_component_names)
+        var_ds = pd.Series(data=np.nan, index=var_component_names)
 
     return var_ds
